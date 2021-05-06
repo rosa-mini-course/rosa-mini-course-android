b5822023a1f0e8f3f57bc8840e04be07f056e33a
sakurafisch <sakurafisch@gmail.com>
2021-05-06 10:58:26 +0800

    feat: fullfill signup button

--- app/src/main/java/com/winnerwinter/myapplication/LoginActivity.java
+++ app/src/main/java/com/winnerwinter/myapplication/LoginActivity.java
@@ -16,7 +16,10 @@ import com.apollographql.apollo.ApolloCall;
 import com.apollographql.apollo.ApolloClient;
 import com.apollographql.apollo.api.Response;
 import com.apollographql.apollo.exception.ApolloException;
+import com.apollographql.apollo.exception.ApolloHttpException;
+import com.apollographql.apollo.exception.ApolloNetworkException;
 import com.winnerwinter.SignInMutation;
+import com.winnerwinter.SignUpMutation;
 
 import org.jetbrains.annotations.NotNull;
 
@@ -30,6 +33,8 @@ public class LoginActivity extends AppCompatActivity {
 //    private TextView registerTextView;
     private ApolloClient apolloClient;
     private UserManager userManager;
+
+    private Button signupButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -91,55 +96,131 @@ public class LoginActivity extends AppCompatActivity {
                 // 此处进行登录验证
                 runOnUiThread(() -> {
                     progressBar.setVisibility(View.VISIBLE);
-                    loginButton.setClickable(false);
+                    loginButton.setEnabled(false);
                 });
                 String username = uidEditText.getText().toString();
                 String password = passwordEditText.getText().toString();
                 SignInMutation mutation = SignInMutation.builder().passwd(password).mail(username).build();
 
-                apolloClient
-                        .mutate(mutation)
-                        .enqueue(new ApolloCall.Callback<SignInMutation.Data>() {
+                try {
+                    apolloClient
+                            .mutate(mutation)
+                            .enqueue(new ApolloCall.Callback<SignInMutation.Data>() {
+                                         @Override
+                                         public void onResponse(@NotNull Response<SignInMutation.Data> response) {
+                                             if (response.hasErrors()) {
+                                                 final BackendError error = ApolloErrorHelper.getBackendError(Objects.requireNonNull(response.getErrors()));
+                                                 runOnUiThread(() -> {
+                                                     progressBar.setVisibility(View.GONE);
+                                                     loginButton.setClickable(true);
+                                                     Toast.makeText(LoginActivity.this, error.message, Toast.LENGTH_SHORT).show();
+                                                 });
+                                                 return;
+                                             }
+
+                                             SignInMutation.SignIn data = Objects.requireNonNull(Objects.requireNonNull(response.getData()).signIn());
+                                             User user = new User(data.userId(), data.useremail());
+                                             userManager.saveUser(user);
+                                             Log.e("user2", user.toString());
+
+                                             runOnUiThread(() -> {
+                                                 progressBar.setVisibility(View.GONE);
+                                                 loginButton.setClickable(true);
+                                                 Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
+                                                 startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                                                 finish();
+                                             });
+                                         }
+                                         //
+                                         @Override
+                                         public void onFailure(@NotNull final ApolloException e) {
+                                             // Lower-level error
+                                             e.printStackTrace();
+                                             runOnUiThread(() -> {
+                                                 Toast.makeText(LoginActivity.this, "登录失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                                 progressBar.setVisibility(View.GONE);
+                                                 loginButton.setEnabled(true);
+                                             });
+                                         }
+                                     }
+                            );
+                } catch (@NotNull final ApolloNetworkException e) {
+                    e.printStackTrace();
+                    runOnUiThread(() -> {
+                        Toast.makeText(LoginActivity.this, "登录失败" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                        progressBar.setVisibility(View.GONE);
+                        loginButton.setEnabled(true);
+                    });
+                }
+
+            }).start();
+        });
+        signupButton = findViewById(R.id.signup);
+        signupButton.setOnClickListener(v -> {
+            if (uidEditText.getText().toString().equals("")) {
+                Toast.makeText(LoginActivity.this, "帐号不能为空！", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            if (passwordEditText.getText().toString().equals("")) {
+                Toast.makeText(LoginActivity.this, "密码不能为空！", Toast.LENGTH_SHORT).show();
+                return;
+            }
+            new Thread(() -> {
+                runOnUiThread(() -> {
+                    progressBar.setVisibility(View.VISIBLE);
+                    signupButton.setEnabled(false);
+                });
+            });
+            String username = uidEditText.getText().toString();
+            String password = passwordEditText.getText().toString();
+            SignUpMutation mutation = SignUpMutation.builder().invCode("5201314").mail(username).passwd(password).rol("student").build();
+            try {
+                apolloClient.mutate(mutation)
+                        .enqueue(new ApolloCall.Callback<SignUpMutation.Data>() {
                             @Override
-                            public void onResponse(@NotNull Response<SignInMutation.Data> response) {
+                            public void onResponse(@NotNull Response<SignUpMutation.Data> response) {
                                 if (response.hasErrors()) {
                                     final BackendError error = ApolloErrorHelper.getBackendError(Objects.requireNonNull(response.getErrors()));
                                     runOnUiThread(() -> {
                                         progressBar.setVisibility(View.GONE);
-                                        loginButton.setClickable(true);
+                                        signupButton.setEnabled(false);
                                         Toast.makeText(LoginActivity.this, error.message, Toast.LENGTH_SHORT).show();
                                     });
-
                                     return;
                                 }
-
-                                SignInMutation.SignIn data = Objects.requireNonNull(Objects.requireNonNull(response.getData()).signIn());
-                                User user = new User(data.userId(), data.useremail());
+                                String useremail = Objects.requireNonNull(response.getData()).signUp().useremail();
+                                String userId = response.getData().signUp().userId();
+                                User user = new User(userId, useremail);
                                 userManager.saveUser(user);
-                                Log.e("user2", user.toString());
-
                                 runOnUiThread(() -> {
                                     progressBar.setVisibility(View.GONE);
-                                    loginButton.setClickable(true);
+                                    signupButton.setEnabled(true);
+                                    Toast.makeText(LoginActivity.this, "注册成功", Toast.LENGTH_SHORT).show();
                                     startActivity(new Intent(LoginActivity.this, MainActivity.class));
                                     finish();
-                                    Toast.makeText(LoginActivity.this, "登录成功", Toast.LENGTH_SHORT).show();
                                 });
+
                             }
-//
+
                             @Override
-                            public void onFailure(@NotNull final ApolloException e) {
-                                // Lower-level error
+                            public void onFailure(@NotNull ApolloException e) {
                                 e.printStackTrace();
                                 runOnUiThread(() -> {
-                                    Toast.makeText(LoginActivity.this, "登录失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
+                                    Toast.makeText(LoginActivity.this, "注册失败：" + e.getMessage(), Toast.LENGTH_SHORT).show();
                                     progressBar.setVisibility(View.GONE);
-                                    loginButton.setClickable(true);
+                                    signupButton.setEnabled(true);
                                 });
                             }
-                        }
-                );
-            }).start();
+                        });
+            } catch (@NotNull ApolloNetworkException e) {
+                e.printStackTrace();
+                runOnUiThread(() -> {
+                    Toast.makeText(LoginActivity.this, "注册失败" + e.getLocalizedMessage(), Toast.LENGTH_SHORT).show();
+                    progressBar.setVisibility(View.GONE);
+                    signupButton.setEnabled(true);
+                });
+            }
+
         });
     }
 
